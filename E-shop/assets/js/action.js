dde56bffe061878a18c95ffcd65ca500deaa4d07
dejvvoci <109139666+dejvvoci@users.noreply.github.com>
2024-07-24 15:32:09 +0200

    rating stars option active

--- E-shop/assets/js/action.js
+++ E-shop/assets/js/action.js
@@ -1,1 +1,15 @@
- 
+document.querySelectorAll('.product').forEach(product => {
+    const stars = product.querySelectorAll('.star i');
+
+    stars.forEach((star, index) => {
+        star.addEventListener('mouseover', () => {
+            stars.forEach((s, i) => {
+                s.classList.toggle('active', i <= index);
+            });
+        });
+
+        star.addEventListener('mouseout', () => {
+            stars.forEach(s => s.classList.remove('active'));
+        });
+    });
+});
